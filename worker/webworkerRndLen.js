df7aa959aa0858d18d71508d20cba31cffffa68d
junichi <sakai.jun01@gmail.com>
2021-07-09 13:04:43 +0200

    UI-Lock

--- worker/webworkerRndLen.js
+++ worker/webworkerRndLen.js
@@ -1,8 +1,8 @@
 //calculate random roll length for every dice
 let val = 1 + Math.floor(Math.random() * 80);
-let counter = 0;
+let counter = Math.floor(Math.random() * 6);
 var diceAttr = {
-	cnt: Math.floor(Math.random() * 6),
+	cnt: 0,
 	finished: false
 };
 var timeout;
